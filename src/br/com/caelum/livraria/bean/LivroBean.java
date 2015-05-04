5090513fd088963b70bf57b537f511f65b0c4e95
john.clemente <john.clemente@johnclemente.foton.local>
2015-05-04 10:20:41 -0300

    Fim do capítulo 2.

---
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -0,0 +1,17 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+	Livro livro = new Livro();
+
+	public Livro getLivro() {
+	    return livro;
+	}
+
+	public void gravar() {
+		System.out.println("Gravando livro!" + this.livro.getTitulo());
+	}
+}
